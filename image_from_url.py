aee033f990dbd7e8ea87653b8f3f4f0aaea16581
naripok <fernando.canteruccio@gmail.com>
2025-02-13 15:52:27 -0300

    return empty pixel if no url

--- image_from_url.py
+++ image_from_url.py
@@ -1,18 +1,17 @@
-from PIL import Image, ImageOps
+import numpy
 import requests
 import torch
-import numpy
+from PIL import Image, ImageOps
+
 
 class ImageFromURL:
     def __init__(self):
         pass
-    
+
     @classmethod
     def INPUT_TYPES(s):
         return {
-            "required": {
-                "url": ("STRING", {"multiline": False})
-            },
+            "required": {"url": ("STRING", {"multiline": False})},
         }
 
     RETURN_NAMES = ("image",)
@@ -22,6 +21,12 @@ class ImageFromURL:
     CATEGORY = "jitcoder"
 
     def fetch_image(self, url):
+        if not url:
+            # Return an empty square transparent image
+            # HWC format (batch_size, dim0, dim1, channels)
+            return (torch.zeros((1, 1, 1, 3), dtype=torch.float32),)
+
+        # Load the image from the url
         image = Image.open(requests.get(url, stream=True).raw)
         image = ImageOps.exif_transpose(image)
         image = image.convert("RGB")
